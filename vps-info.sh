b62db9e041995d33305980155436a51d80840ccb
JustADeve <142085654+JustADeve@users.noreply.github.com>
2024-05-04 14:23:38 +0200

    Update vps-info.sh

--- vps-info.sh
+++ vps-info.sh
@@ -5,13 +5,14 @@ avg_cpu_today=$(top -bn1 | awk '/^%Cpu/{print 100-$8}')
 
 # Get memory usage
 mem_total=$(awk '/MemTotal/{print $2}' /proc/meminfo)
-mem_used=$(awk '/MemAvailable/{print $2}' /proc/meminfo)
-mem_usage=$(echo "scale=2; (($mem_total - $mem_used) / $mem_total) * 100" | bc -l)
+mem_used=$(awk '/MemFree/{print $2}' /proc/meminfo)
+mem_cached=$(awk '/^Cached:/{print $2}' /proc/meminfo)
+mem_buffered=$(awk '/Buffers/{print $2}' /proc/meminfo)
+mem_free=$((mem_used + mem_cached + mem_buffered))
+mem_usage=$(echo "scale=2; ($mem_total - $mem_free) / $mem_total * 100" | bc -l)
 
 # Get disk usage
-disk_total=$(df -h --output=size / | sed 1d)
-disk_used=$(df -h --output=used / | sed 1d | tr -d " ")
-disk_usage=$(echo "scale=2; ($disk_used / $disk_total) * 100" | bc -l)
+disk_usage=$(df -h --output=pcent / | sed 1d | tr -d ' %')
 
 # Get network usage
 rx_speed=$(cat /sys/class/net/eth0/statistics/rx_bytes)
@@ -32,8 +33,8 @@ system_info=$(uname -a)
 echo "VPS Information:"
 echo "--------------"
 echo "Average CPU Usage Today: ${avg_cpu_today}%"
-echo "Memory Usage: ${mem_usage}% (${mem_used} kB / ${mem_total} kB)"
-echo "Disk Usage: ${disk_usage}% (${disk_used} used out of ${disk_total})"
+echo "Memory Usage: ${mem_usage}% (${mem_used} kB used / ${mem_total} kB total)"
+echo "Disk Usage: ${disk_usage}%"
 echo "Network Usage (RX): ${rx_speed} KB/s"
 echo "Network Usage (TX): ${tx_speed} KB/s"
 echo "Uptime: ${uptime}"
